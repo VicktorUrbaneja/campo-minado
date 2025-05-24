66edf79e5fcfbbe0e9e5f2f76ef0b23f86199cea
Vicktor Urbaneja <vicktorurbaneja@hotmail.com>
2025-05-24 08:33:56 -0300

    feat: novos metodos classe Campo

---
+++ src/main/java/com/vicktordev/cm/exceptions/ExplosaoException.java
@@ -0,0 +1,11 @@
+package com.vicktordev.cm.exceptions;
+
+import java.io.Serial;
+
+public class ExplosaoException extends RuntimeException {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+
+}
--- src/main/java/com/vicktordev/cm/model/Campo.java
+++ src/main/java/com/vicktordev/cm/model/Campo.java
@@ -1,5 +1,7 @@
 package com.vicktordev.cm.model;
 
+import com.vicktordev.cm.exceptions.ExplosaoException;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,6 +39,35 @@ public class Campo {
         } else {
             return false;
         }
-
     }
+
+    void alternarMarcacao() {
+        if (!aberto) {
+            marcado = !marcado;
+        }
+    }
+
+    boolean abrir() {
+
+        if (!aberto && !marcado) {
+            aberto = true;
+
+            if (minado) {
+                throw new ExplosaoException();
+            }
+
+            if (vizinhancaSegura()) {
+                vizinhos.forEach(v -> v.abrir());
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    boolean vizinhancaSegura() {
+        return vizinhos.stream().noneMatch(v -> v.minado);
+    }
+
+
 }
