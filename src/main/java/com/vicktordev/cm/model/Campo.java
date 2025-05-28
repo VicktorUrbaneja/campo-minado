e5f388cb887c2c822b3734997b9ce6873ddd50cb
Vicktor Urbaneja <vicktorurbaneja@hotmail.com>
2025-05-28 11:05:13 -0300

    feat: finalização classe Campo e CampoTeste

--- src/main/java/com/vicktordev/cm/model/Campo.java
+++ src/main/java/com/vicktordev/cm/model/Campo.java
@@ -73,6 +73,10 @@ public class Campo {
         minado = true;
     }
 
+    public boolean isMinado() {
+        return minado;
+    }
+
     public boolean isMarcado() {
         return marcado;
     }
@@ -85,5 +89,42 @@ public class Campo {
         return !isAberto();
     }
 
+    public int getLinha() {
+        return linha;
+    }
 
+    public int getColuna() {
+        return coluna;
+    }
+
+    boolean objetivoAlcancado() {
+        boolean desvendado = !minado && aberto;
+        boolean protegido = minado && marcado;
+        return desvendado || protegido;
+    }
+
+    long minasNaVizinhanca() {
+        return vizinhos.stream().filter(v -> v.minado).count();
+    }
+
+    void reiniciar() {
+        aberto = false;
+        minado = false;
+        marcado = false;
+    }
+
+    @Override
+    public String toString() {
+        if (marcado) {
+            return "x";
+        } else if (aberto && minado) {
+            return "*";
+        } else if (aberto && minasNaVizinhanca() > 0) {
+            return Long.toString(minasNaVizinhanca());
+        } else if (aberto) {
+            return " ";
+        } else {
+            return "?";
+        }
+    }
 }
--- test/java/com/vicktordev/cm/model/CampoTeste.java
+++ test/java/com/vicktordev/cm/model/CampoTeste.java
@@ -136,4 +136,104 @@ public class CampoTeste {
         assertTrue(campo22.isAberto() && campo11.isFechado());
     }
 
+    @Test
+    void testeGetLinha() {
+        campo.getLinha();
+        assertEquals(3, 3);
+    }
+
+    @Test
+    void testeGetColuna() {
+        campo.getColuna();
+        assertEquals(3, 3);
+    }
+
+    @Test
+    void testeObjetivoAlcancadoDesvendado() {
+        campo.abrir();
+        assertTrue(campo.objetivoAlcancado());
+    }
+
+    @Test
+    void testeObjetivoAlcancadoProtegido() {
+        campo.minar();
+        campo.alternarMarcacao();
+        assertTrue(campo.objetivoAlcancado());
+    }
+
+    @Test
+    void testeMinasNaVizinhanca() {
+        Campo campo11 = new Campo(1, 1);
+        Campo campo12 = new Campo(1, 2);
+        campo12.minar();
+
+        Campo campo22 = new Campo(2, 2);
+        campo22.adicionarVizinho(campo11);
+        campo22.adicionarVizinho(campo12);
+
+        campo.adicionarVizinho(campo22);
+        campo.abrir();
+
+        assertEquals(1, campo22.minasNaVizinhanca());
+    }
+
+    @Test
+    void testeReiniciar() {
+        campo.reiniciar();
+
+        assertFalse(campo.isAberto());
+        assertFalse(campo.isMarcado());
+        assertFalse(campo.isMinado());
+    }
+
+    @Test
+    void testToStringMarcado() {
+        campo.alternarMarcacao();
+        assertEquals("x", campo.toString());
+    }
+
+    @Test
+    void testToStringAbertoMinado() {
+        campo.minar();
+
+        assertThrows(ExplosaoException.class, () -> { //aqui quando o campo abrir vai jogar exception, tenho que checar a exception e output
+            campo.abrir();
+        });
+        assertEquals("*", campo.toString());
+    }
+
+    @Test
+    void testToStringAbertoMinasNaVizinhanca() {
+        Campo campo11 = new Campo(1, 1);
+        Campo campo12 = new Campo(1, 2);
+        campo12.minar();
+
+        Campo campo22 = new Campo(2, 2);
+        campo22.adicionarVizinho(campo11);
+        campo22.adicionarVizinho(campo12);
+
+        campo.adicionarVizinho(campo22);
+        campo.abrir();
+        assertEquals("1", campo22.toString());
+    }
+
+    @Test
+    void testToStringAberto() {
+        Campo campo11 = new Campo(1, 1);
+        Campo campo12 = new Campo(1, 2);
+        campo12.minar();
+
+        Campo campo22 = new Campo(2, 2);
+        campo22.adicionarVizinho(campo11);
+        campo22.adicionarVizinho(campo12);
+
+        campo.adicionarVizinho(campo22);
+        campo.abrir();
+        assertEquals(" ", campo.toString());
+    }
+
+    @Test
+    void testeToStringFechado() {
+        assertEquals("?", campo.toString());
+    }
 }
